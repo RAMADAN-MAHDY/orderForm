67c27cae94db5e6cf4acbaeb4b34aa58a982d926
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2024-07-04 20:05:02 +0300

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Royal corner",
   description: "للتسويق الالكتروني",
-  googleٍiteVerification : "-1HDfMA8r2MPfNp6oa5PTR1Pe2Z-g4CThr_hEn1rIsM",
 };
 
 {/* <meta name="google-site-verification" content="-1HDfMA8r2MPfNp6oa5PTR1Pe2Z-g4CThr_hEn1rIsM" /> */}
@@ -14,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="an" rel="rtl">
+        <head>
+        <meta name="google-site-verification" content="-1HDfMA8r2MPfNp6oa5PTR1Pe2Z-g4CThr_hEn1rIsM" />
+        </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
