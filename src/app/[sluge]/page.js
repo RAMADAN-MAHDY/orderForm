29e35a45ca12db3c67493d1fcc021d54beb9ae6c
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2024-07-22 00:30:09 +0300

    update

--- src/app/[sluge]/page.js
+++ src/app/[sluge]/page.js
@@ -78,20 +78,59 @@ const DataComponent = ({ params }) => {
   }
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        //https://api-order-form.onrender.com
-        const response = await fetch(`https://api-order-form.vercel.app/condition/${params.sluge}`);
-        const responseData = await response.json();
-        setData(responseData);
-        setIsLoading(false);
-        // console.log(responseData);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+    // const fetchData = async () => {
+    //   try {
+    //     //https://api-order-form.onrender.com
+    //     const response = await fetch(`https://api-order-form.vercel.app/condition/${params.sluge}`);
+    //     const responseData = await response.json();
+    //     setData(responseData.data);
+    //     setIsLoading(false);
+    //     // console.log(responseData);
+    //   } catch (error) {
+    //     console.error('Error fetching data:', error);
+    //   }
+    // };
 
-    fetchData();
+    // fetchData();
+
+
+
+    async function fetchData(id, page, limit) {
+        try {
+          const response = await fetch(`https://api-order-form.vercel.app/condition/${id}?page=${page}&limit=${limit}`);
+          const data = await response.json();
+          return data;
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      }
+      
+      async function fetchAllData(id) {
+        let page = 1;
+        const limit = 10;
+        let allData = [];
+      
+        while (true) {
+          const data = await fetchData(id, page, limit);
+          if (!data) break; // إضافة تحقق للتأكد من أن البيانات موجودة
+          allData = allData.concat(data.data);
+          
+          if (page >= data.totalPages) {
+            break;
+          }
+          page++;
+        }
+      
+        console.log('All data:', allData);
+        setData(allData)
+        setIsLoading(false);
+      }
+      
+      // استبدل `yourId` بالمعرف الفعلي الذي تريد استخدامه
+      fetchAllData(params.slug);
+
+
+
   }, [params]);
 
   return (
